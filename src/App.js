7f6bdc309d7d406d0dea810ac10ac397509c812e
Mike Oram <Mikeoram01@gmail.com>
2022-11-08 18:54:47 +0000

    setup containers

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import Container from "./Components/Container/Container";
 
 function App() {
   return (
     <div className="App">
       <h1 className={"clock-title"}>25 + 5 Clock</h1>
+        <Container className={"settings-container"}>
+          <Container className={"break-settings-container"}></Container>
+          <Container className={"session-settings-container"}></Container>
+        </Container>
     </div>
   );
 }
