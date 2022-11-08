08041c24c10c0ae6f7a9a625f4c6f32e75082d3b
Mike Oram <Mikeoram01@gmail.com>
2022-11-08 19:01:48 +0000

    added the labels

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import './App.css';
 import Container from "./Components/Container/Container";
+import Label from "./Components/Label/Label";
 
 function App() {
   return (
     <div className="App">
       <h1 className={"clock-title"}>25 + 5 Clock</h1>
         <Container className={"settings-container"}>
-          <Container className={"break-settings-container"}></Container>
-          <Container className={"session-settings-container"}></Container>
+          <Container className={"break-settings-container"}>
+            <Label id={"break-label"} text={"Break"}/>
+          </Container>
+          <Container className={"session-settings-container"}>
+            <Label id={"session-label"} text={"Session"}/>
+          </Container>
         </Container>
     </div>
   );
---
+++ src/Components/Label/Label.js
@@ -0,0 +1,7 @@
+const Label = (props) => {
+    return (
+        <p className={"label"} id={props.id}>{props.text}</p>
+    )
+}
+
+export default Label
