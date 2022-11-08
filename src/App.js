0f4d3af1e066a15073423284b2124a9a6b6e0519
Mike Oram <Mikeoram01@gmail.com>
2022-11-08 19:16:21 +0000

    Added the Decrementor Component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
 import './App.css';
 import Container from "./Components/Container/Container";
 import Label from "./Components/Label/Label";
+import {useState} from "react";
 
 function App() {
+
+  const [breakLength, setBreakLength] = useState(5)
+
   return (
     <div className="App">
       <h1 className={"clock-title"}>25 + 5 Clock</h1>
         <Container className={"settings-container"}>
           <Container className={"break-settings-container"}>
             <Label id={"break-label"} text={"Break"}/>
+            <Container className={"increment-decrement-container"}>
+
+            </Container>
           </Container>
           <Container className={"session-settings-container"}>
             <Label id={"session-label"} text={"Session"}/>
+            <Container className={"increment-decrement-container"}>
+
+            </Container>
           </Container>
         </Container>
     </div>
---
+++ src/Components/Decrementor/Decrementor.js
@@ -0,0 +1,5 @@
+const Decrementor = () => {
+
+}
+
+export default Decrementor
